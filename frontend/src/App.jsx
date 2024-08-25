95e29fb6a5f24843c42526ed72b55528b14fcf1c
premchand <premchand6055@gmail.com>
2024-08-25 20:57:30 +0530

    Added Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/about' element = {<About/>} />
+        <Route path='/cart' element = {<Cart/>} />
+        <Route path='/collection' element = {<Collection/>} />
+        <Route path='/contact' element = {<Contact/>} />
+        <Route path='/login' element = {<Login/>} />
+        <Route path='/orders' element = {<Orders/>} />
+        <Route path='/placeorder' element = {<PlaceOrder/>} />
+        <Route path='/product' element = {<Product/>} />
+      </Routes>
+    </div>
   )
 }
 
